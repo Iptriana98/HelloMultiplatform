b17fac41a4dc7286982b70e4b475922366dbf1b7
ibrahinperez <iptrianaa@gmail.com>
2024-05-28 19:59:05 -0500

    added Rocket data component

---
+++ shared/src/commonMain/kotlin/RocketComponent.kt
@@ -0,0 +1,46 @@
+/*
+ * Copyright (c)  Created by Ibrahin Perez. All rights reserved.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ */
+
+import io.ktor.client.HttpClient
+import io.ktor.client.call.body
+import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
+import io.ktor.client.request.get
+import io.ktor.serialization.kotlinx.json.json
+import kotlinx.datetime.Instant
+import kotlinx.datetime.TimeZone
+import kotlinx.datetime.toLocalDateTime
+import kotlinx.serialization.json.Json
+
+class RocketComponent {
+    private val httpClient = HttpClient {
+        install(ContentNegotiation) {
+            json(Json {
+                prettyPrint = true
+                isLenient = true
+                ignoreUnknownKeys = true
+            })
+        }
+    }
+
+    private suspend fun getDateOfLastSuccessfulLaunch(): String {
+        val rockets: List<RocketLaunch> = httpClient.get("https://api.spacexdata.com/v4/launches").body()
+        val lastSuccessLaunch = rockets.last { it.launchSuccess == true }
+        val date = Instant.parse(lastSuccessLaunch.launchDateUTC)
+            .toLocalDateTime(TimeZone.currentSystemDefault())
+
+        return "${date.month} ${date.dayOfMonth}, ${date.year}"
+    }
+
+    suspend fun launchPhrase(): String =
+        try {
+            "The last successful launch was on ${getDateOfLastSuccessfulLaunch()} 🚀"
+        } catch (e: Exception) {
+            println("Exception during getting the date of the last successful launch $e")
+            "Error occurred"
+        }
+}
