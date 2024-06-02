434fa2414f6ad2a54faee5a20dfd073832affb53
ibrahinperez <iptrianaa@gmail.com>
2024-06-01 21:02:41 -0500

    - configured main screen in android project

--- shared/src/commonMain/kotlin/Greeting.kt
+++ shared/src/commonMain/kotlin/Greeting.kt
@@ -1,15 +1,23 @@
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
 import kotlin.random.Random
+import kotlin.time.Duration.Companion.seconds
 
 class Greeting {
     private val platform: Platform = getPlatform()
+    private val rocketComponent = RocketComponent()
 
     /**
      * Returns a greeting for the given platform.
      *
      */
-    fun greet(): String {
-        val firstWord = if (Random.nextBoolean()) "Hi!" else "Hello!"
-
-        return "$firstWord Guess what this is! > ${platform.name.reversed()}!"
+    fun greet(): Flow<String> = flow {
+        emit(if (Random.nextBoolean()) "Hi!" else "Hello!")
+        delay(1.seconds)
+        emit("Guess what this is! > ${platform.name.reversed()}")
+        delay(1.seconds)
+        emit(daysPhrase())
+        emit(rocketComponent.launchPhrase())
     }
 }
